616a6565af33b8dd11d07fd4856af8b4d4875bcb
Konstantin Kolesnikov <kol42@mail.ru>
2022-11-03 19:24:05 +0700

    Two commit

---
+++ WindowsFormsApp11/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp11
+{
+    class Customer
+    {
+        private readonly string _name;
+        public string Name { get => _name; }
+
+        public Customer(string name)
+        {
+            _name = name;
+        }
+
+    }
+}
---
+++ WindowsFormsApp11/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+        Queue<Customer> queue;
+        public Form1()
+        {
+            InitializeComponent();
+            queue = new Queue<Customer>();
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var name = textBox1.Text;
+            Customer user = new Customer(name);
+            queue.Enqueue(user);
+            ShowUser();
+            textBox1.Text = String.Empty;
+        }
+        private void ShowUser()
+        {
+            listBox1.Items.Clear();
+            foreach (var item in queue)
+            {
+                listBox1.Items.Add(item.Name);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Customer user = queue.Dequeue();
+            MessageBox.Show("Чек №");
+            ShowUser();
+        }
+    }
+}
